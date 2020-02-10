76d96cdd94c894c56b62263acc64e90188f43f14
Keshav <vkjayendravarma@gmail.com>
2020-02-10 20:51:50 +0530

    file upload function ready

---
+++ app.py
@@ -0,0 +1,52 @@
+#imports to handle local files 
+import os
+
+#imports to handle server 
+from flask import Flask, request, render_template
+
+
+#init flask
+app = Flask(__name__)
+
+# APP configs
+app.config["IMAGES_TO_IDENTIFY"] = "static/images"
+app.config["IMAGE_ID"] = 0
+
+# Directory setup
+if not os.path.exists(app.root_path + app.config["IMAGES_TO_IDENTIFY"] ):
+    os.mkdir(app.config["IMAGES_TO_IDENTIFY"])
+
+# Index route
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Route to handle image uploads
+@app.route("/uploadimage", methods=["POST"])
+def uploadimage():
+    
+    if request.files :            
+        images_input = request.files["images"] 
+            
+        # Changing file names
+        app.config["IMAGE_ID"] = app.config["IMAGE_ID"] + 1
+        f_name, f_ext  = os.path.splitext(images_input.filename) 
+        f_name = str(app.config["IMAGE_ID"])            
+        filename = f_name + f_ext
+            
+        #Save images to local directory
+        images_input.save(os.path.join(app.root_path, app.config["IMAGES_TO_IDENTIFY"], filename))
+            
+        print("Image saved" + filename)
+            
+    
+    return {"filename": filename}  #return filename as response 
+    
+    
+# @app.route("process_images/:images", methods=["POST"])
+# def process_images():
+    
+
+# Server options 
+if __name__ == "__main__":
+    app.run(debug=True)
